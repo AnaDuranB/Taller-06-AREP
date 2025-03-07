f762f0fb0e445d0150c42e5421544bc216609255
unknown <ana.duran-b@labinfo.is.escuelaing.edu.co>
2025-03-07 10:02:32 -0500

    inicial

---
+++ src/main/java/arep/taller6/taller6/SecureWeb.java
@@ -0,0 +1,13 @@
+package arep.taller6.taller6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWeb {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWeb.class, args);
+	}
+
+}
---
+++ src/test/java/arep/taller6/taller6/Taller6ApplicationTests.java
@@ -0,0 +1,13 @@
+package arep.taller6.taller6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
